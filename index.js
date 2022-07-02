09df12aa8112eebac3920150a0f1d12b18e52dff
HumairaRahman <humairamitul@gmail.com>
2022-07-02 12:32:55 +0600

    deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: "http://localhost:3000",
+  cors: "*",
 });
 
 const PORT = process.env.PORT || 5000;
@@ -24,21 +24,17 @@ mongoose
   .then(() => console.log("Conntected To MongoDB"));
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/search", require("./routes/search"));
 app.use("/api/chat", require("./routes/chat"));
 app.use("/api/message", require("./routes/message"));
+
+app.get('/', (req, res) => {
+  res.json("HELLO")
+})
 
 server.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
